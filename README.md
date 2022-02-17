d464da7208c6a5240aef1bea879cdddd9395bc6a
0-krish <65729597+0-krish@users.noreply.github.com>
2022-02-16 21:45:00 -0500

    Emails & customized sales tax rate

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 
 ## shopping-cart
 
-### How do I run the game?
+### How do I run the system?
 - Open the command line interface
-- Activate game-specific conda environment with Python 3.8:
+- Activate a conda environment specific to the shopping-cart system with Python 3.8:
   - ```conda create -n shopping-env Python=3.8```
   - ```conda activate shopping-env```
 - Install required packages using pip and "requirements.txt" file:
   - ```pip install -r requirements.txt```
-- Run game using command:
+- Run system using command:
   - ```python shopping_cart.py```
 
-### Testing
+### How do I customize the tax rate?
 
-To run tests on the logic that decides the winner of
-the game, type the following command into the command
-line interface:
-- ```pytest```
+To customize the tax rate to your store's location:
+- Create a file with the following name: ".env"
+- In the file, enter your desired tax rate as shown in the example below:
+  - ```TAX_RATE = 0.085```
+  - Ensure you use the exact format as prescribed above
+  - Enter your tax rate as a decimal number between 0 and 1
+  - The default tax rate is 8.75%
+  - Ensure the ".env" file is in the same directory as the rest of these files
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,7 +1,20 @@
+#
 # shopping_cart.py
+#
+# Shopping Cart Exercise
+#
+# Author: Krish Sarawgi
+#   Github: "0-krish"
+#   NetID: ks1730
 
-# import to display date and time at checkout
+
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
 
 products = [
     {
@@ -54,6 +67,7 @@ print("Welcome to the Green Foods Grocery Checkout System!")
 subtotal_price = 0
 selected_ids = []
 stored_ids = []
+matching_products = []
 
 print("Type 'DONE' once all desired items have been scanned.")
 print("---------------------------------")
@@ -88,14 +102,78 @@ for selected_id in selected_ids:
     subtotal_price = subtotal_price + matching_product["price"]
     print("#>  ... " + matching_product["name"] + " (" + to_usd(matching_product["price"]) + ")")
 
-tax_rate = 0.0875  # hard-coded New York City sales tax percentage
+tax_rate = float(os.getenv("TAX_RATE", default="0.0875"))
 tax_amount = tax_rate * subtotal_price
 total_price = subtotal_price + tax_amount
 
 print("#> ---------------------------------")
 print("#> SUBTOTAL:", to_usd(subtotal_price))
-print("#> TAX: " + to_usd(tax_amount))
+print(f"#> TAX ({tax_rate*100:,.2f}%): " + to_usd(tax_amount))
 print("#> TOTAL: " + to_usd(total_price))
 print("#> ---------------------------------")
+
+# Receipt by email
+
+receipt_preference = input("Would the customer like a copy of the receipt to be sent to them via email? [y/n]: ")
+receipt_preference.lower()
+
+if receipt_preference == "y" or receipt_preference == "yes":
+
+    # code adapted from https://github.com/prof-rossetti/intro-to-python/blob/main/notes/python/packages/sendgrid.md
+
+    customer_email = input("Please enter the customer's email ID: ")
+
+    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+    SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+
+    client = SendGridAPIClient(SENDGRID_API_KEY)  # > <class 'sendgrid.sendgrid.SendGridAPIClient>
+
+    subject = "Your Receipt from the Green Grocery Store"
+
+    html_content = ""
+
+    html_content += "---------------------------------<br>\n"
+    html_content += "GREEN FOODS GROCERY<br>\n"
+    html_content += "WWW.GREEN-FOODS-GROCERY.COM<br>\n"
+    html_content += "---------------------------------<br>\n"
+    html_content += "CHECKOUT AT:"
+    html_content += str(today_string)
+    html_content += "\n<br>---------------------------------<br>\n"
+    html_content += "PRODUCTS PURCHASED:<br>\n"
+
+    for selected_id in selected_ids:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        html_content += "  ... " + matching_product["name"] + " (" + to_usd(matching_product["price"]) + ")<br>\n"
+
+    html_content += "---------------------------------<br>"
+    html_content += "SUBTOTAL:"
+    html_content += str(to_usd(subtotal_price))
+    html_content += f"\n<br>TAX ({tax_rate*100:,.2f}%): "
+    html_content += str(to_usd(tax_amount))
+    html_content += "\n<br>TOTAL: "
+    html_content += str(to_usd(total_price))
+    html_content += "\n<br>---------------------------------"
+    html_content += "\n<br>THANK YOU FOR SHOPPING WITH US. COME AGAIN!"
+    html_content += "\n<br>---------------------------------"
+
+
+    print(html_content)
+
+    message = Mail(
+        from_email=SENDER_ADDRESS,
+        to_emails=customer_email,
+        subject=subject,
+        html_content=html_content)
+
+    try:
+        response = client.send(message)
+        if str(response.status_code) == "202":
+            print("Email to", customer_email, "sent!")
+
+    except Exception as err:
+        print(type(err))
+        print(err)
+
 print("#> THANKS, SEE YOU AGAIN SOON!")
 print("#> ---------------------------------")
